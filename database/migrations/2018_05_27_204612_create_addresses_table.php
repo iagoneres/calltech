ace56eaae8c22d1d6bb5319c8fce7d61a07ef0f8
Iago Neres <iagoneresb@gmail.com>
2018-05-27 18:10:46 -0300

    Migrations | Create migrations for addresses and bank accounts, Update table users

---
+++ database/migrations/2018_05_27_204612_create_addresses_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('postal_code', 8);
+            $table->string('street', 300);
+            $table->integer('number')->nullable();
+            $table->string('neighborhood', 200);
+            $table->string('city', 100);
+            $table->string('state', 2);
+            $table->string('country', 2)->default('BR');
+            $table->string('complement')->nullable();
+
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
